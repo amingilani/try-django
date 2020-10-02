17480797514a05c101b318cd759a903afa79ed5c
Amin Shah Gilani <amin@gilani.me>
2020-10-01 22:24:42 -0400

    Recreate db with new migrations

--- products/models.py
+++ products/models.py
@@ -2,9 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    title = models.TextField()
-    description = models.TextField()
-    price = models.TextField()
-    summary = models.TextField(
-        default="this is cool! I'm making incremental migrations based off of a model"
-    )
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=10000)
+    summary = models.TextField()
